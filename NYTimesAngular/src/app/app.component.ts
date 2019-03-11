bbaef5708823cb934d2dc8ab8e444497a0329070
Phelo Macanda <42580766+PheloMacanda@users.noreply.github.com>
2019-03-11 03:26:21 +0200

    Angular folder
    uploaded the folder with angular files

---
+++ NYTimesAngular/src/app/app.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+@NgModule({
+...
+imports: [BrowserAnimationsModule],
+...
+})
+
+
+export class AppComponent {
+  title = 'NYTimesApi';
+  
+
+  value = {};
+
+  constructor(){}
+
+  ngOnInit(){
+
+  }
+
+  getValues(){
+
+    this.http.get('http://localhost:4200/api/values').subscribe
+                 ( response => this.value = response.json());
+  }
+
+
+  public NYTimes{
+
+      get{
+
+          return NYTimes.Current;
+      }
+  }
+
+
+
+
+}
